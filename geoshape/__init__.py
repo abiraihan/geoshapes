4cde53862ba0258a11cbbc613f16910b5bcc99f1
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-04 18:16:43 -0400

    Updates
     - Update v.0.0.5

--- geoshape/__init__.py
+++ geoshape/__init__.py
@@ -1,2 +1,1 @@
 import geoshape
-import exception
--- geoshape/geoShape.py
+++ geoshape/geoShape.py
@@ -12,11 +12,36 @@ import shapely
 import geopandas
 import itertools
 import pathlib
-import exception
 import rasterio
 import scipy
 from pykrige.ok import OrdinaryKriging
 from sklearn.neighbors import KNeighborsRegressor
+
+class ShapeError(Exception):
+    
+    def __init__(self, geoms:str):
+        
+        self.geoms = geoms
+        self.message = f"\n{self.geoms} is not acceptable.\nOnly 'Square', 'Rectangle', 'Square-Rectangle' are acceptable geometry shape to process"
+        super().__init__(self.message)
+
+class RangeError(Exception):
+    
+    def __init__(self, distance, reason):
+        
+        self.distance = distance
+        self.reason = reason
+        self.message = f"\nReason -- {self.reason} : Out of range. Assigned distance should be at least less than {self.distance} to identify a zones"
+        super().__init__(self.message)
+
+class InputError(Exception):
+    
+    def __init__(self, conditions, reasons):
+        
+        self.conditions = conditions
+        self.reasons = reasons
+        self.message = f"\nReason -- {self.reasons} : |'{self.conditions}'| isn't acceptable. Check Input parameters"
+        super().__init__(self.message)
 
 class splitShape:
     
