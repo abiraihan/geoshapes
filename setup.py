1e9d55aea5e1bfd67f60ead35d6e431fe208fc49
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 01:34:19 -0400

    Update setup.py
    -ups

--- setup.py
+++ setup.py
@@ -12,11 +12,6 @@ setup(
   keywords = ['experimental design', 'spatial analysis', 'interpolation'],   # Keywords that define your package best
   install_requires=[
       'numpy',
-      'rasterio',
-      'pathlib',
-      'scipy',
-      'sklearn',
-      'pykrige',
       ],
   classifiers=[
     'Development Status :: 1 - Planning',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
