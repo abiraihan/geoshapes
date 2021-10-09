7e7f5c5354ca9ac857ae78c4e97b88ad231a3c61
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 02:48:16 -0400

    Update setup.py
    ups

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'geoshapes',         # How you named your package folder (MyLib)
   packages = ['geoshapes'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A basic geospatial geoprocessing library on experimental design',   # Give a short description about your library
   author = 'Abir Raihan',                   # Type in your name
