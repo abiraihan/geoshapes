ce58bf38f836041af5fe7f0e0838cd31435fe8bc
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 01:57:05 -0400

    Update setup.py
    updates

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
       'shapely',
       'itertools',
       'pathlib',
-      
+      'geoshapes',
       ],
   classifiers=[
     'Development Status :: 1 - Planning',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
