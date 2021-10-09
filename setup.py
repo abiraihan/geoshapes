9910bb7d59b2fe4866cd190ee07550b81cbcea5d
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 00:13:06 -0400

    Update setup.py
    - Updated

--- setup.py
+++ setup.py
@@ -2,22 +2,25 @@ from distutils.core import setup
 setup(
   name = 'geoshape',         # How you named your package folder (MyLib)
   packages = ['geoshape'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A basic geospatial data sciene on experimental design',   # Give a short description about your library
   author = 'Abir Raihan',                   # Type in your name
   author_email = 'abirraihan.urp@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/abiraihan/geoshape',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/abiraihan/geodesigns/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/abiraihan/geoshape/archive/refs/tags/geoshapeV0.0.1-alphaPlanning.tar.gz',    # I explain this later on
   keywords = ['experimental design', 'spatial analysis', 'interpolation'],   # Keywords that define your package best
   install_requires=[
+      'math',
       'numpy',
       'shapely',
       'pandas',
       'geopandas',
       'rasterio',
+      'pathlib',
       'scipy',
       'sklearn',
+      'pykrige',
       ],
   classifiers=[
     'Development Status :: 1 - Planning',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
