ffae14648d0fd84eb5645f37f8e77d0d9667d9d6
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 01:22:51 -0400

    Update __init__.py
    -updated

--- geoshapes/__init__.py
+++ geoshapes/__init__.py
@@ -1,9 +1,9 @@
-from geoshapes.geoshape import utils
-from geoshapes.geoshape import gridShape
-from geoshapes.geoshape import splitShape
-from geoshapes.geoshape import checkShape
-from geoshapes.geoshape import mergeShape
-from geoshapes.geoshape import selectGeom
-from geoshapes.geoshape import interpolation
-from geoshapes.geoshape import interpolationShape
+from geoshapes.geoshapes import utils
+from geoshapes.geoshapes import gridShape
+from geoshapes.geoshapes import splitShape
+from geoshapes.geoshapes import checkShape
+from geoshapes.geoshapes import mergeShape
+from geoshapes.geoshapes import selectGeom
+from geoshapes.geoshapes import interpolation
+from geoshapes.geoshapes import interpolationShape
 from geoshapes.exception import InputError, RangeError, ShapeError
