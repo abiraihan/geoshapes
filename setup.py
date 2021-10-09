6ee841437c8d1a03de6b85b1010ac6638f416802
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-09 00:02:25 -0400

    -Added Setup.cfg
    - Updated

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+setup(
+  name = 'geoshape',         # How you named your package folder (MyLib)
+  packages = ['geoshape'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A basic geospatial data sciene on experimental design',   # Give a short description about your library
+  author = 'Abir Raihan',                   # Type in your name
+  author_email = 'abirraihan.urp@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/abiraihan/geoshape',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/abiraihan/geodesigns/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['experimental design', 'spatial analysis', 'interpolation'],   # Keywords that define your package best
+  install_requires=[
+      'numpy',
+      'shapely',
+      'pandas',
+      'geopandas',
+      'rasterio',
+      'scipy',
+      'sklearn',
+      ],
+  classifiers=[
+    'Development Status :: 1 - Planning',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
