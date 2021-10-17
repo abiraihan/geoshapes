a6b6ee03d212969d12d436d04c2ec67a5d7a3b7b
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-17 11:06:03 -0400

    _added_test

---
+++ example/app-test.py
@@ -0,0 +1,14 @@
+import shapely, geopandas
+from geoshapes import splitShape
+
+pointLocation = shapely.geometry.Point(0.0,0.0)
+circle = splitShape.splitCircle(
+    geoms = pointLocation,
+    circleRadius = 50,
+    incrementDegree = 5,
+    clipInterior = True,
+    innerWidth = 30,
+    getGeom = 'Both')
+gdf = geopandas.GeoDataFrame(geometry = circle)
+
+print(f"Total row number is geodataframe is {len(gdf)}")
