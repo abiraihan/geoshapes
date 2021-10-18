ae425632ace1511946a1860387e7a578144bda10
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-18 12:35:11 -0400

    Update app-test.py

--- testCodes/app-test.py
+++ testCodes/app-test.py
@@ -9,9 +9,15 @@ circle = splitShape.splitCircle(
     clipInterior = True,
     innerWidth = 30,
     getGeom = 'Both')
-gdf = geopandas.GeoDataFrame(geometry = circle)
 
+gdf = geopandas.GeoDataFrame(geometry = circle[::4])
 print(f"Total row number is geodataframe is {len(gdf)}")
+
+geos = [splitShape.splitGeom(i, 4, rotation = 120) for i in gdf.geometry]
 
 areas = sum([i.area*1e10/4046.86 for i in gdf.geometry])
 print(f"Total ares of the geometry is {round(areas, 2)} acre")
+ft = pandas.concat([i for i in geos])
+area = sum([i.area*1e10/4046.86 for i in ft.geometry])
+print(f"Total ares of the geometry is {round(area, 2)} acre")
+    
