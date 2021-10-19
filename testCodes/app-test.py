5d99bfca04e4b596905d5576dd8e71d9bad65069
ABIR RAIHAN <abirraihan.urp@gmail.com>
2021-10-19 09:08:46 -0400

    Update app-test.py

--- testCodes/app-test.py
+++ testCodes/app-test.py
@@ -1,4 +1,4 @@
-import shapely, geopandas
+import shapely, geopandas, pandas
 from geoshapes import splitShape
 
 pointLocation = shapely.geometry.Point(0.0,0.0)
@@ -11,12 +11,11 @@ circle = splitShape.splitCircle(
     getGeom = 'Both')
 
 gdf = geopandas.GeoDataFrame(geometry = circle[::4])
-print(f"Total row number is geodataframe is {len(gdf)}")
+print(f"Total row number of geodataframe is : {len(gdf)}")
 
 geos = [splitShape.splitGeom(i, 4, rotation = 120) for i in gdf.geometry]
-
 areas = sum([i.area*1e10/4046.86 for i in gdf.geometry])
-print(f"Total ares of the geometry is {round(areas, 2)} acre")
+print(f"Total ares of the geometry is : {round(areas, 2)} acre")
 ft = pandas.concat([i for i in geos])
 area = sum([i.area*1e10/4046.86 for i in ft.geometry])
-print(f"Total ares of the geometry is {round(area, 2)} acre")
+print(f"Total ares of the geometry is : {round(area, 2)} acre")
